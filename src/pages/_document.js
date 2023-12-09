097c381c9bd0b91beffef58270cf7e36d5484391
Ryoma0305 <ryomaaa0602@gmail.com>
2023-12-09 17:50:04 +0900

    google fonts読み込み

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
-      <body>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="flex flex-col">
+        <Header />
         <Main />
+        {/* <Footer /> */}
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
