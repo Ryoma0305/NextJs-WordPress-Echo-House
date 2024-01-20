5d1e99cfd03d3fbbff341228525e96c40bfddf08
Ryoma0305 <ryomaaa0602@gmail.com>
2024-01-20 22:28:34 +0900

    contactページを追加

---
+++ src/pages/contact/index.js
@@ -0,0 +1,32 @@
+import ContactForm from "../../components/contact/contact-form";
+
+export default function Form() {
+  return (
+    <main>
+      <div className="flex h-40 items-center justify-center bg-gradient-right-pink md:h-80">
+        <h2 className="font-accent text-xl font-bold uppercase md:text-4xl">Contact</h2>
+      </div>
+      <div className="container mx-auto mt-5 max-w-[1200px] px-4 md:px-56 md:pb-40 md:pt-24">
+        {/* <form onSubmit={registerUser}>
+          <div className="mb-3">
+            <label htmlFor="email">メールアドレス</label>
+            <input id="email" name="email" type="email" className="form-control" placeholder="name@example.com" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="message" className="form-label">
+              問合せ内容
+            </label>
+            <textarea id="message" name="message" className="form-control" rows="3"></textarea>
+          </div>
+          <div className="mb-3">
+            <button type="submit" className="btn btn-primary">
+              送信
+            </button>
+          </div>
+        </form> */}
+
+        <ContactForm />
+      </div>
+    </main>
+  );
+}
