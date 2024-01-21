28e61be1e81612a6c0007b3f7f32ebce78158772
Ryoma0305 <ryomaaa0602@gmail.com>
2024-01-21 17:19:24 +0900

    スクロールしたらトップへ戻るボタンを表示するように修正

--- src/components/common/BackToTopButton.js
+++ src/components/common/BackToTopButton.js
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react";
+
 const BackToTopButton = () => {
+  const [isButtonActive, setIsButtonActive] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollWindow);
+    return () => {
+      window.removeEventListener("scroll", scrollWindow);
+    };
+  }, []);
+
+  const scrollWindow = () => {
+    const top = 100;
+    let scroll = 0;
+    scroll = window.scrollY;
+    if (top <= scroll) {
+      setIsButtonActive(true);
+    } else {
+      setIsButtonActive(false);
+    }
+  };
+
   return (
-    <div className="fixed bottom-0 right-0 opacity-100 transition-opacity duration-500 ease-out-expo" id="toTop" aria-hidden="false">
+    <div
+      className={isButtonActive ? "fixed bottom-0 right-0 opacity-100 transition-opacity duration-500 ease-out-expo" : "fixed bottom-0 right-0 opacity-0 transition-opacity duration-500 ease-out-expo"}
+      id="toTop"
+      aria-hidden="false"
+    >
       <div className="inline-flex bg-gradient-green text-center text-white-100">
         <a href="#" className="flex flex-col items-center gap-1 p-3 text-xs">
           <span className="h-0 w-0 border-x-[8px] border-b-[10px] border-t-0 border-solid border-[transparent_transparent_#fff_transparent]"></span>
