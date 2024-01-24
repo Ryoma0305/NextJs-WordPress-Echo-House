2304f60d01f05599e042cb15cd067fbda7b8381f
Ryoma0305 <ryomaaa0602@gmail.com>
2024-01-24 16:07:36 +0900

    クラス名修正

--- src/components/top/about/index.js
+++ src/components/top/about/index.js
@@ -4,10 +4,10 @@ import SectionHeading from "../../../components/common/sectionHeading";
 const About = () => {
   return (
     <FadeInBottom>
-      <section className="about relative bg-[url('/images/1floor-6.jpg')] bg-cover bg-center bg-no-repeat px-4 py-16 before:absolute before:inset-0 before:bg-about-bg" id="about">
-        <div className="about-textwrap relative">
+      <section className="relative bg-[url('/images/1floor-6.jpg')] bg-cover bg-center bg-no-repeat px-4 py-16 before:absolute before:inset-0 before:bg-about" id="about">
+        <div className="relative">
           <SectionHeading titleJp="コンセプト" titleEn="About us" />
-          <p className="about-text text-center leading-loose">
+          <p className="text-center leading-loose">
             エコーハウスは新今宮駅まで徒歩5分、通天閣、新世界まで徒歩7分、と大阪の中心地に位置し、
             <br />
             2018年にリニューアルオープンした女性専用シェアハウスです。
