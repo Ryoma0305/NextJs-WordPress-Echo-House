5d6379423f6714c07e2aeec0430e53617584c67e
Ryoma0305 <ryomaaa0602@gmail.com>
2024-01-28 02:05:27 +0900

    エラー修正

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
+import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
