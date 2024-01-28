a15c6e406fa9736862e6d3970f8c239a24459553
Ryoma0305 <ryomaaa0602@gmail.com>
2024-01-28 10:53:23 +0900

    スタイル修正

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -5,8 +5,8 @@ import ContactForm from "../../components/contact/ContactForm";
 export default function Form() {
   return (
     <Layout>
-      <div className="flex h-40 items-center justify-center bg-gradient-right-pink md:h-80">
-        <h2 className="font-accent text-xl font-bold uppercase md:text-4xl">Contact</h2>
+      <div className="flex h-40 items-center justify-center bg-slate-800 md:h-80">
+        <h1 className="font-accent text-xl font-bold uppercase text-white-100 md:text-4xl">Contact</h1>
       </div>
       <div className="container mx-auto mt-5 max-w-[1200px] px-4 md:px-56 md:pb-40 md:pt-24">
         <ContactForm />
